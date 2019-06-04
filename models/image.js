5d7f8a0088b4775528f3033db043cdea31db048d
V1shvesh <22243137+V1shvesh@users.noreply.github.com>
2019-06-04 11:21:31 +0530

    (model/image.js) Add key field to Image model

--- models/image.js
+++ models/image.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const Image = mongoose.model('Image', new mongoose.Schema({
   name: String,
   size: Number,
+  key: String,
   fileURL: String,
   timestamp: { type: Date, default: Date.now },
 }));
